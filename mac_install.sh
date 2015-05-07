ac6e6eeb9773d491e5fe72d4d1df0f0608838ef7
rombob <rombob@gmail.com>
2015-05-07 01:03:05 -0400

    mac install script

---
+++ mac_install.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+sudo echo "Installing PyXAPI"
+cd /tmp
+curl -O http://www.pps.univ-paris-diderot.fr/~ylg/PyXAPI/PyXAPI-0.1.tar.gz
+tar -xzvf PyXAPI-0.1.tar.gz
+cd PyXAPI-0.1
+./configure
+make
+sudo make install
+cd ..
+
+echo "Installing autossh"
+brew update
+brew install autossh
+
+echo "Getting sshuttle"
+git clone https://github.com/pthrasher/sshuttle.git /opt/sshuttle
+cd /opt/sshuttle/src/
+echo "alias sshuttle=$(pwd)/sshuttle" >> $HOME/.bash_profile
